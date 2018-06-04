54d4d824c9ad5624c397a9acce7dbb68461745b3
thomhos <thom.hos@gmail.com>
2018-06-04 11:48:37 +0200

    use custom server

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// import express from 'express';
-// import next from 'next';
-// import { env } from './lib';
+import express from 'express';
+import next from 'next';
+import { env } from './lib';
 
-// const port = env.PORT || 3000;
-// const dev = env.NODE_ENV !== 'production';
+const port = env.PORT || 3000;
+const dev = env.NODE_ENV !== 'production';
 
-// const app = next({ dev, dir: __dirname });
-// const server = express();
+const app = next({ dev, dir: __dirname });
+const server = express();
 
-// app.prepare().then(() => {
-//   server.get('*', app.getRequestHandler());
+app.prepare().then(() => {
+  server.get('*', app.getRequestHandler());
 
-//   server.listen(port, (err: Error) => {
-//     if (err) throw err;
-//     console.log(`> Ready on http://localhost:${port}`);
-//   });
-// });
+  server.listen(port, (err: Error) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
