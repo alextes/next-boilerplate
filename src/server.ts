064027659868eabffe2765275fbb9fb77f84b15c
thomhos <thom.hos@gmail.com>
2018-06-04 11:33:57 +0200

    add some types / and boilerplatey things

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+// require('dotenv').config();
+
+// import express from 'express';
+// import next from 'next';
+// import { env } from './lib';
+
+// const port = env.PORT || 3000;
+// const dev = env.NODE_ENV !== 'production';
+
+// const app = next({ dev, dir: __dirname });
+// const server = express();
+
+// app.prepare().then(() => {
+//   server.get('*', app.getRequestHandler());
+
+//   server.listen(port, (err: Error) => {
+//     if (err) throw err;
+//     console.log(`> Ready on http://localhost:${port}`);
+//   });
+// });
